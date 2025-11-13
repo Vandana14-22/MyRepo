9eaac36ff8b1c5e055cf001a7a9de3b60c1ff5db
Vandana14-22 <bandarupallivandana@gmail.com>
2025-11-13 14:45:11 +0530

    Update app.java

--- app.java
+++ app.java
@@ -2,3 +2,10 @@ Hello
   Just want to test this repo whether it getting automatic or not after commit.
 
     Thank you
+
+    Above condition is failed and now I have changed the webhook configuation to everywhere
+    Let see whether it success for all pipelines or single pipe.
+
+    Thank you!
+  
+
